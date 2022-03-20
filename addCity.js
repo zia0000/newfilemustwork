c4b35802ff05fca7023cdfd5c90170da2e873abd
Gia <kgkim774@gmail.com>
2022-03-21 07:05:40 +0900

    dsfsfwef

--- addCity.js
+++ addCity.js
@@ -1,19 +1,19 @@
-function addCity(cityName ,image, content) {
+function addItem(cityName ,image, content) {
     const div = document.createElement("div");
-    div.classList.add("container");
+    div.classList.add("card");
     div.innerHTML = `
             <img src="${image}" alt="${city}" />
-            <p>${content}</p>
+            <span>${content}</span>
         `;
-    document.querySelector("#contains").appendChild(div);
+    document.querySelector("#container").appendChild(div);
 
-    const sideSpan = document.createElement("sideSpan");
-    sideSpan.innerHTML = cityName;
-    document.querySelector("#sidebar").appendChild(sideSpan);
+    const span = document.createElement("span");
+    span.innerHTML = cityName;
+    document.querySelector("#sidebar").appendChild(span);
 }
 
-function addHandle(event){
-    event.preventDefault();
+function addHandle(e){
+    e.preventDefault();
     const cityName = document.querySelector(#cityName).value;
     const image = document.querySelector(#image).value;
     const content = document.querySelector(#content).value;
@@ -21,4 +21,4 @@ function addHandle(event){
     closeModal();
 }
 
-document.querySelector(#addCityForm).addEventListener("submit", addHandle)
+document.getElementById("addItemForm").addEventListener("submit", addItemHandle)
