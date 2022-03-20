85c7f41e6c4db6bf5348885e8da6d10fb1a11797
Gia <kgkim774@gmail.com>
2022-03-21 07:29:40 +0900

    ㄴㅇㄹㄴㅇ

--- addCity.js
+++ addCity.js
@@ -1,4 +1,4 @@
-function addItem(cityName, image, content) {
+function addCity(cityName, image, content) { //add city
     const div = document.createElement("div");
     div.classList.add("card");
     div.innerHTML = `
@@ -12,14 +12,14 @@ function addItem(cityName, image, content) {
     document.querySelector("#sidebar").appendChild(span);
 }
 
-function addItemHandle(event){
+function addCityHandle(event){
     event.preventDefault();
     const cityName = document.getElementById("cityName").value;
     const image = document.getElementById("image").value;
     const content = document.getElementById("content").value;
-    addItem(cityName, image, content);
+    addCity(cityName, image, content); //addItem
     closeModal();
 }
 
 
-document.getElementById("addItemForm").addEventListener("submit", addItemHandle);
+document.getElementById("addCityForm").addEventListener("submit", addCityHandle); //Iten>City
